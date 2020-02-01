9b0f2a9e133aa49863d6a154bcde5f59b63062ea
Andres Ortega <adresortegao@gmail.com>
2020-02-01 00:37:16 -0400

    Agregar flecha de direccion que se mueve

--- src/Player/Direction/Direction.js
+++ src/Player/Direction/Direction.js
@@ -4,11 +4,25 @@ class Direction {
   constructor(sprite) {
 	this.sprite = sprite;
 	this.sprite.rotation = this.sprite.rotation;
+	this.sprite.setOrigin(1);
+	this.clockwise = true;
   }
 
   update(x, y) {
 	this.sprite.x = x + 15;
-	this.sprite.y = y - 70;
+	this.sprite.y = y - 50;
+
+	if(this.clockwise) {
+	  this.sprite.rotation += Math.PI/80;
+	  if(this.sprite.rotation >= Math.PI/2) {
+		this.clockwise = false;
+	  }
+	} else {
+	  this.sprite.rotation -= Math.PI/80;
+	  if(this.sprite.rotation <= 0) {
+		this.clockwise = true;
+	  }
+	}
   }
 }
 
