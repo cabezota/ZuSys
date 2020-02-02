4aaca9bb4fe6964a5c6edca67b205e2608c78c8f
ItsMeAndrea <itsandrea_94@hotmail.com>
2020-02-02 13:36:22 -0400

    audio update

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -1,5 +1,8 @@
 import Phaser from "phaser";
 import bgGameOver from "../assets/fullBackground.png";
+import gameOverAudio from "../assets/audio/GameOver.ogg";
+
+var overAudio;
 
 export class GameOver extends Phaser.Scene {
   constructor() {
@@ -7,9 +10,12 @@ export class GameOver extends Phaser.Scene {
   }
   preload() {
     this.load.image("bgGameOver", bgGameOver);
+    this.load.audio("overAudio", gameOverAudio);
   }
   create() {
     this.bgGameOver = this.add.image(250, 300, "bgGameOver");
+    overAudio = this.sound.add("overAudio");
+    overAudio.play();
 
     //this.scene.remove("Level1");
     this.scene.remove("Hud");
